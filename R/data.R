ce9b1815afac61e18ccc617b0b2c8f46fa77b0e1
Nick Giangreco <npg2108@cumc.columbia.edu>
2021-01-01 16:32:15 -0500

    initial commit

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @importFrom data.table data.table
+NULL
+
+#' report_dat
+#'
+#' Catetgorization of reports for a drug and adverse event
+#' within the FDA's adverse event reporting system for pediatric patients
+#' defined within the NICHD defined developmental stages
+#'
+#' the drug indicator is for reporting of Methylphenidate and
+#' the event indicator is for the reporting of Paranoia
+#'
+#' @format A data frame with five variables: \code{safetyreportid}, \code{age},
+#'   \code{year}, \code{D} and \code{E}.
+"report_dat"
